51901e1fa1cb0cc35c62d7e5657b2a43e9111eb4
itay241199@gmail.com <itay241199@gmail.com>
2022-11-14 19:44:25 +0200

    ee

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ const logger = (req, res, next) => {
     next();
     
 }
+const logger1 = (req, res, next) => {
+    const date = new Date().toTimeString();
+     console.log(date);
+    next();
+    
+}
+const logger2 =(req,res,next)=>{
+    const date = new Date().toDateString();
+    console.log(date,'hello');
+    next();
+}
 
 app.get('/', logger,(req, res)=> {
     res.send('ok')
+})
+app.get('/13', logger1,(req, res)=> {
+    res.send('itay')
+})
+app.get('/12',logger2,(req,res)=>{
+    res.send('hello')
 })
 
 
